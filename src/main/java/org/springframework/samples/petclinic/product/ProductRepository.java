ee7e6b76c515bd2e14ff97aee6618474112bd1fa
Ernesto <ertorivero2661@gmail.com>
2022-11-01 20:35:51 +0100

    Control check

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,14 +3,23 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product, Integer>{
+    
     List<Product> findAll();
+    @Query("SELECT pt FROM ProductType pt")
     List<ProductType> findAllProductTypes();
     Optional<Product> findById(int id);
+    @Query("SELECT pt FROM ProductType pt WHERE pt.name == :name")
     Product findByName(String name);
     Product save(Product p);
+
+    @Query("SELECT pt FROM ProductType pt WHERE pt.name = :name")
+    ProductType getProductType(String name);
+    @Query("Select p FROM Product p WHERE p.price<:price")
+    List<Product> findByPriceLessThan(double price);
 }
